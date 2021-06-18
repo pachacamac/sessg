2c5c18ad98f860af3c994ec1ca1ef329c30857e4
Marc <pachacamac@users.noreply.github.com>
2021-06-18 13:53:55 +0200

    Update robots.txt-page.js

--- example_site/src/robots.txt-page.js
+++ example_site/src/robots.txt-page.js
@@ -1,3 +1,7 @@
+// This shows how to create non-html pages.
+// Simply add the extension you like in front like so `filename.<ext>-page.js`.
+// The default when you only use `filename.page.js` results in `filename.html`.
+
 module.exports = function(){
   return html`
   User-agent: *
